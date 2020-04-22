872475a0037529ffd30d6e2f27715cf3490cf127
SeregaDeveloper <48175873+SeregaDeveloper@users.noreply.github.com>
2020-04-22 12:59:57 +0300

    Update TrafGraph.py

--- TrafGraph.py
+++ TrafGraph.py
@@ -64,10 +64,10 @@ def Make_Graph_Edges():
     return 0
 #---------------------------------------------------------
 def Draw_Graph():
-    """ Function which is draw the """
+    """ Function which is draw the graph"""
     pos = nx.circular_layout(G)
     edge_labels = {(u, v): d['weight'] for u, v, d in G.edges(data=True)}
-    nx.draw_networkx_nodes(G, pos, node_size=200, node_color='green')
+    nx.draw_networkx_nodes(G, pos, node_size=2, node_color='green')
     nx.draw_networkx_edges(G, pos)
     nx.draw_networkx_labels(G, pos)
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='r')

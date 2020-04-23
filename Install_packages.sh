03e68d6f01dd6bd084f4d28b134927cf6b49efbd
SeregaDeveloper <48175873+SeregaDeveloper@users.noreply.github.com>
2020-04-23 21:19:34 +0300

    Update Install_packages.sh

--- Install_packages.sh
+++ Install_packages.sh
@@ -1,10 +1,10 @@
 # !/bin/sh
 
-echo "Установка необходимых компонентов...";
+echo "Please wait...";
 sleep 1;
 sudo apt-get install python3-pip;
 pip3 install pandas;
 pip3 install networkx;
 pip3 install matplotlib;
 sudo apt-get install python-tk;
-echo "Установка завершена!";
+echo "Installation complete!";
